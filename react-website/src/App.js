99f5465972e4062f40fbcbed9c6cf1cd1053b1a4
Brian <bmatzel1@binghamton.edu>
2023-02-07 03:07:20 -0500

    trying to make the male/female button

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -3,12 +3,14 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import InitialForm from "./components/InitialForm";
+import { ButtonTest } from "./components/ButtonTest";
 
 export default function App() {
   return (
     <main>
       <Header />
       <InitialForm/>
+      {/* <ButtonTest /> */}
       <Home />
       <Footer />
     </main>
