14aa0ff5396361e16f7c4b4b72a9861cb5ea8a3b
Brian Matzelle <brianmatzelle@Brians-MacBook-Air-2.local>
2023-02-08 20:44:00 -0500

    made placeholders for pages

--- nextjs-website/src/app/about/page.tsx
+++ nextjs-website/src/app/about/page.tsx
@@ -1,3 +1,5 @@
 export default function AboutUsPage() {
-    
+    return (
+        <h1 className="h-2/3 flex justify-center p-60 bg-white text-black">About us placeholder</h1>
+    );
 }
--- nextjs-website/src/app/signup/page.tsx
+++ nextjs-website/src/app/signup/page.tsx
@@ -1,3 +1,5 @@
 export default function SignUpPage() {
-    
+    return (
+        <h1 className="h-2/3 flex justify-center p-60 bg-white text-black">Sign up placeholder</h1>
+    );
 }
