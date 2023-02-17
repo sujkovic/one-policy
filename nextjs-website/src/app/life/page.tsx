b9a2ddb056733d55bd7916fcaf4e281cef9014c6
Brian Matzelle <brianmatzelle@Brians-MacBook-Air-2.local>
2023-02-17 17:48:52 -0500

    buttons fully functional

--- nextjs-website/src/app/life/page.tsx
+++ nextjs-website/src/app/life/page.tsx
@@ -29,93 +29,110 @@ function getAge(dateString: string) {
 const LifeForm: React.FC = () => {
   // M == male, F == female
   const [gender, setGender] = useState('M');
+  const [maleActive, setMaleActive] = useState(true);
+  const [femaleActive, setFemaleActive] = useState(false);
 
   return (
     <div className={styles.main}>
-      <div className={styles.form}>
-        <Formik<InitialValues>
-          initialValues={{
-            dobMonth: '',
-            dobDay: '',
-            dobYear: '',
-            zipCode: '',
-          }}
-          onSubmit={(values, actions) => {
-            const valuesSoFar: Values = {
-              gender: gender,
-              age: getAge(values.dobYear + "-" + values.dobMonth + "-" + values.dobDay),
-              zipCode: values.zipCode,
-              term_len: 0,
-              face_amt: 0,
-              tobacco: ''
-            }
-            actions.setSubmitting(false);
-            console.log(valuesSoFar);
-          }}
-        >
-          {({ isSubmitting }) => (
-            <Form>
-              <div className={styles.genderGroup}>
-                <button
-                  onClick={() => {
-                    setGender('M')
-                  }} 
-                  className={styles.buttonLeft}>
-                    Male
-                </button>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Tell us about you.</h1>
+        <div className={styles.form}>
+          <Formik<InitialValues>
+            initialValues={{
+              dobMonth: '',
+              dobDay: '',
+              dobYear: '',
+              zipCode: '',
+            }}
+            onSubmit={(values, actions) => {
+              const valuesSoFar: Values = {
+                gender: gender,
+                age: getAge(values.dobYear + "-" + values.dobMonth + "-" + values.dobDay),
+                zipCode: values.zipCode,
+                term_len: 0,
+                face_amt: 0,
+                tobacco: ''
+              }
+              actions.setSubmitting(false);
+              console.log(valuesSoFar);
+            }}
+          >
+            {({ isSubmitting }) => (
+              <Form>
+                <div className={styles.genderGroup}>
+                  <button
+                    onClick={() => {
+                      setGender('M');
+                      setMaleActive(true);
+                      setFemaleActive(false);
+                    }}
+                    className={
+                      maleActive ? 
+                      `${styles.buttonLeftFocus} ${styles.buttonLeft}` 
+                      : 
+                      styles.buttonLeft}>
+                      Male
+                  </button>
 
-                <button
-                  onClick={() => {
-                    setGender('F')
-                  }} 
-                  className={styles.buttonRight}>
-                    Female
-                </button>
-
-              </div>
-
-              <div>
-                <label htmlFor="dobMonth">Date of Birth:</label>
-                <Field type="text" name="dobMonth">
-                  {({ field }: { field: any }) => (
-                    <div>
-                      <input id="dobMonth" {...field} placeholder="MM" />
-                      <ErrorMessage name='dobMonth' />
-                    </div>
-                  )}
-                </Field>
-                <Field type="text" name="dobDay">
-                  {({ field }: { field: any }) => (
-                    <div>
-                      <input id="dobDay" {...field} placeholder="DD" />
-                      <ErrorMessage name='dobDay' />
-                    </div>
-                  )}
-                </Field>
-                <Field type="text" name="dobYear">
-                  {({ field }: { field: any }) => (
-                    <div>
-                      <input id="dobYear" {...field} placeholder="YYYY" />
-                      <ErrorMessage name='dobYear' />
-                    </div>
-                  )}
-                </Field>
-            </div>
-            <Field type="text" name="zipCode">
-              {({ field }: { field: any }) => (
-                <div>
-                  <label htmlFor="zipCode">ZIP Code:</label>
-                  <input id="zipCode" {...field} />
-                  <ErrorMessage name='zipCode' />
+                  <button
+                    onClick={() => {
+                      setGender('F');
+                      setMaleActive(false);
+                      setFemaleActive(true);
+                    }} 
+                    className={
+                      femaleActive ? 
+                      `${styles.buttonRightFocus} ${styles.buttonRight}` 
+                      : 
+                      styles.buttonRight
+                      }>
+                      Female
+                  </button>
                 </div>
-              )}
-            </Field>
-            <button type="submit" disabled={isSubmitting}>
-              Continue
-            </button>
-          </Form>
-        )}
-        </Formik>
+
+                <div>
+                  <label htmlFor="dobMonth">Date of Birth:</label>
+                  <Field type="text" name="dobMonth">
+                    {({ field }: { field: any }) => (
+                      <div>
+                        <input id="dobMonth" {...field} placeholder="MM" />
+                        <ErrorMessage name='dobMonth' />
+                      </div>
+                    )}
+                  </Field>
+                  <Field type="text" name="dobDay">
+                    {({ field }: { field: any }) => (
+                      <div>
+                        <input id="dobDay" {...field} placeholder="DD" />
+                        <ErrorMessage name='dobDay' />
+                      </div>
+                    )}
+                  </Field>
+                  <Field type="text" name="dobYear">
+                    {({ field }: { field: any }) => (
+                      <div>
+                        <input id="dobYear" {...field} placeholder="YYYY" />
+                        <ErrorMessage name='dobYear' />
+                      </div>
+                    )}
+                  </Field>
+              </div>
+              <Field type="text" name="zipCode">
+                {({ field }: { field: any }) => (
+                  <div>
+                    <label htmlFor="zipCode">ZIP Code:</label>
+                    <input id="zipCode" {...field} />
+                    <ErrorMessage name='zipCode' />
+                  </div>
+                )}
+              </Field>
+              <button type="submit" disabled={isSubmitting}>
+                Continue
+              </button>
+            </Form>
+          )}
+          </Formik>
+        </div>
       </div>
     </div>
   );
